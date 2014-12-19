90a564d00ffba4220c39d1efd5c56c0a3ed1b68e
gnarmstr <gnarmstr@hotmail.com>
2014-12-20 08:48:27 +1100

    Master

---
+++ FormMain.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Vixen_SMS
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.textBoxUID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxPWD = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBoxLogFileName = new System.Windows.Forms.TextBox();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.timerCheckMail = new System.Windows.Forms.Timer(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxReplaceText = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxOutputSequence = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxSequenceTemplate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxVixenServer = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "POP3 Server:";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(176, 46);
+            this.textBoxServer.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(684, 31);
+            this.textBoxServer.TabIndex = 1;
+            // 
+            // textBoxUID
+            // 
+            this.textBoxUID.Location = new System.Drawing.Point(176, 85);
+            this.textBoxUID.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxUID.Name = "textBoxUID";
+            this.textBoxUID.Size = new System.Drawing.Size(684, 31);
+            this.textBoxUID.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "UID:";
+            // 
+            // textBoxPWD
+            // 
+            this.textBoxPWD.Location = new System.Drawing.Point(176, 121);
+            this.textBoxPWD.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPWD.Name = "textBoxPWD";
+            this.textBoxPWD.PasswordChar = '*';
+            this.textBoxPWD.Size = new System.Drawing.Size(684, 31);
+            this.textBoxPWD.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "PWD:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxPWD);
+            this.groupBox1.Controls.Add(this.textBoxServer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxUID);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(892, 181);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Mail Server";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(558, 794);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(164, 58);
+            this.buttonStart.TabIndex = 7;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(740, 794);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(164, 58);
+            this.buttonStop.TabIndex = 8;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBoxLogFileName);
+            this.groupBox2.Controls.Add(this.listBoxLog);
+            this.groupBox2.Location = new System.Drawing.Point(12, 443);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(892, 333);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Log";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 290);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 25);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Log File:";
+            // 
+            // textBoxLogFileName
+            // 
+            this.textBoxLogFileName.Location = new System.Drawing.Point(124, 287);
+            this.textBoxLogFileName.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxLogFileName.Name = "textBoxLogFileName";
+            this.textBoxLogFileName.Size = new System.Drawing.Size(736, 31);
+            this.textBoxLogFileName.TabIndex = 12;
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.HorizontalScrollbar = true;
+            this.listBoxLog.ItemHeight = 25;
+            this.listBoxLog.Location = new System.Drawing.Point(28, 44);
+            this.listBoxLog.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(832, 229);
+            this.listBoxLog.TabIndex = 0;
+            // 
+            // timerCheckMail
+            // 
+            this.timerCheckMail.Interval = 1000;
+            this.timerCheckMail.Tick += new System.EventHandler(this.timerCheckMail_Tick);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.textBoxReplaceText);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBoxOutputSequence);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.textBoxSequenceTemplate);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.textBoxVixenServer);
+            this.groupBox3.Location = new System.Drawing.Point(12, 198);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox3.Size = new System.Drawing.Size(892, 223);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Vixen Info";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(28, 156);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(169, 25);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Text to Replace:";
+            // 
+            // textBoxReplaceText
+            // 
+            this.textBoxReplaceText.Location = new System.Drawing.Point(244, 154);
+            this.textBoxReplaceText.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxReplaceText.Name = "textBoxReplaceText";
+            this.textBoxReplaceText.Size = new System.Drawing.Size(616, 31);
+            this.textBoxReplaceText.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 119);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 25);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Output Sequence:";
+            // 
+            // textBoxOutputSequence
+            // 
+            this.textBoxOutputSequence.Location = new System.Drawing.Point(244, 115);
+            this.textBoxOutputSequence.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxOutputSequence.Name = "textBoxOutputSequence";
+            this.textBoxOutputSequence.Size = new System.Drawing.Size(616, 31);
+            this.textBoxOutputSequence.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(28, 83);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 25);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Template Folder:";
+            // 
+            // textBoxSequenceTemplate
+            // 
+            this.textBoxSequenceTemplate.Location = new System.Drawing.Point(244, 79);
+            this.textBoxSequenceTemplate.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxSequenceTemplate.Name = "textBoxSequenceTemplate";
+            this.textBoxSequenceTemplate.Size = new System.Drawing.Size(616, 31);
+            this.textBoxSequenceTemplate.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 44);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(141, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Vixen Server:";
+            // 
+            // textBoxVixenServer
+            // 
+            this.textBoxVixenServer.Location = new System.Drawing.Point(244, 42);
+            this.textBoxVixenServer.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxVixenServer.Name = "textBoxVixenServer";
+            this.textBoxVixenServer.Size = new System.Drawing.Size(616, 31);
+            this.textBoxVixenServer.TabIndex = 3;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 872);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.Text = "Vixen SMS";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.TextBox textBoxUID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxPWD;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.Timer timerCheckMail;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxOutputSequence;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxSequenceTemplate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxVixenServer;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxReplaceText;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBoxLogFileName;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Runtime.InteropServices;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenPop;
+using OpenPop.Pop3;
+using System.Web;
+
+namespace Vixen_SMS
+{
+    public partial class FormMain : Form
+    {
+        string[] BadWords =
+        {
+            "shit", 
+            "dick",
+            "fuck",
+            "condom",
+            "tit",
+            "pussy",
+            "cunt",
+            "ass",
+            "fart",
+            "rape",
+            "condoms",
+            "sex"
+        };
+
+        Pop3Client pop = new Pop3Client();
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            StartChecking();
+        }
+
+        void StartChecking()
+        {
+            buttonStart.Enabled = false;
+            buttonStop.Enabled = true;
+            timerCheckMail.Interval = 500;
+            timerCheckMail.Enabled = true;
+        }
+
+        private bool Pop3Login()
+        {
+            try
+            {
+                var server = textBoxServer.Text;
+                var uid = textBoxUID.Text;
+                var pwd = textBoxPWD.Text;
+                listBoxLog.Items.Insert(0, "Connecting to: " + server);
+                Application.DoEvents();
+                pop.Connect(server, 995, true);
+                listBoxLog.Items.Insert(0, "Authenticating: " + uid);
+                Application.DoEvents();
+                pop.Authenticate(uid, pwd, AuthenticationMethod.UsernameAndPassword);
+                listBoxLog.Items.Insert(0, "Logged Into Server");
+                Application.DoEvents();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                listBoxLog.Items.Insert(0, "Pop Error: " + ex.Message);
+                return false;
+            }
+        }
+
+        private void timerCheckMail_Tick(object sender, EventArgs e)
+        {
+            timerCheckMail.Interval = 60000;
+            try
+            {
+                if (Pop3Login())
+                {
+                    int messageCount = pop.GetMessageCount();
+                    listBoxLog.Items.Insert(0, "Message Count: " + messageCount);
+                    //var msgList = new List<int>();
+                    for (int messageNum = 1; messageNum <= messageCount; messageNum++)
+                    {
+                        var header = pop.GetMessageHeaders(messageNum);
+                        listBoxLog.Items.Insert(0,
+                            "Retrieved Header # " + messageNum.ToString() + ": " + header.Subject.ToString());
+                        if (header.Subject.Contains("SMS from"))
+                        {
+                            var msg = pop.GetMessage(messageNum);
+                            var phoneNumber = header.Subject.Substring(9).Trim();
+
+                            try
+                            {
+                                var smsMessage = msg.MessagePart.GetBodyAsText();
+                                var msgLines = smsMessage.Split('\r');
+                                smsMessage = msgLines[0];
+                                listBoxLog.Items.Insert(0, "SMS Message: " + smsMessage);
+                                pop.DeleteMessage(messageNum);
+                                // We only want one message at a time so, disconnect and wait for next time.
+                                pop.Disconnect();
+                                SendMessageToVixen(smsMessage);
+                                SendReturnText(phoneNumber, header.From.ToString());
+                                return;
+                            }
+                            catch (Exception ex)
+                            {
+                                listBoxLog.Items.Insert(0, "Error Parsing Message Body: " + ex.Message);
+                            }
+                        }
+                        Application.DoEvents();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                listBoxLog.Items.Insert(0, ex.Message.ToString());
+            }
+            finally
+            {
+                if (pop.Connected)
+                {
+                    try
+                    {
+                        pop.Disconnect();
+                    }
+                    catch
+                    {
+
+                    }
+                }
+                listBoxLog.Items.Insert(0, "Disconnected");
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timerCheckMail.Enabled = false;
+            buttonStart.Enabled = true;
+            buttonStop.Enabled = false;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            XMLProfileSettings profile = new XMLProfileSettings();
+            textBoxServer.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "POP3Server", "pop.gmail.com");
+            textBoxUID.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "UID", "");
+            textBoxPWD.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "Password", "");
+            textBoxVixenServer.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "VixenServer", "http://localhost:8080/api/play/playSequence");
+            textBoxSequenceTemplate.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "SequenceTemplate", "C:\\OneDrive\\Christmas Lights\\Sequences\\Hello.tim");
+            textBoxOutputSequence.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "OutputSequence", "C:\\OneDrive\\Christmas Lights\\Sequences\\HelloOut.tim");
+            textBoxReplaceText.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "ReplaceText", "NamePlaceholder");
+            textBoxLogFileName.Text = profile.GetSetting(XMLProfileSettings.SettingType.Profiles, "LogFile", "C:\\OneDrive\\Christmas Lights\\Vixen SMS\\Vixen SMS\\Log.txt");
+
+            buttonStop.Enabled = false;
+            StartChecking();
+        }
+
+        private void SendMessageToVixen(string msg)
+        {
+            try
+            {
+                var outputFileName = textBoxOutputSequence.Text.ToString();
+                var inputFolderName = textBoxSequenceTemplate.Text.ToString();
+
+                var files = Directory.GetFiles(inputFolderName, "Hello?.tim");
+                var random = new Random();
+                var fileNum = random.Next(1, files.Count());
+                var inputFileName = inputFolderName + "\\Hello" + fileNum.ToString() + ".tim";
+
+                var textToReplace = textBoxReplaceText.Text.ToString();
+                var fileText = File.ReadAllText(inputFileName);
+                if (!HasBadWords(msg))
+                {
+                    msg = msg.Replace("&", "&amp;");
+                    fileText = fileText.Replace(textToReplace, msg);
+                    File.Delete(outputFileName);
+                    File.WriteAllText(outputFileName, fileText);
+                    var url = textBoxVixenServer.Text.ToString() + "?name=" + WebUtility.UrlEncode(outputFileName);
+                    var result = new WebClient().DownloadString(url);
+                    listBoxLog.Items.Insert(0, "Vixen Started: + " + result);
+                    Log(msg);
+                }
+            }
+            catch (Exception ex)
+            {
+                listBoxLog.Items.Insert(0, ex.Message);
+                Log("Error: " + ex.Message);
+            }
+        }
+
+        private void Log(string st)
+        {
+            var logFileName = textBoxLogFileName.Text;
+            File.AppendAllText(logFileName, DateTime.Now.ToString("g") + " " + st + "\r\n");
+        }
+        
+        private bool HasBadWords(string msg)
+        {
+            foreach (string word in BadWords)
+            {
+                if (msg.Contains(word))
+                {
+                    listBoxLog.Items.Insert(0, "Bad Words Detected!");
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            XMLProfileSettings profile = new XMLProfileSettings();
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "POP3Server", textBoxServer.Text);
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "UID", textBoxUID.Text);
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "Password", textBoxPWD.Text);
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "VixenServer", textBoxVixenServer.Text);
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "SequenceTemplate", textBoxSequenceTemplate.Text);
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "OutputSequence", textBoxOutputSequence.Text);
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "ReplaceText", textBoxReplaceText.Text);
+            profile.PutSetting(XMLProfileSettings.SettingType.Profiles, "LogFile", textBoxLogFileName.Text);
+        }
+
+        private void SendReturnText(string phoneNumber, string msgTo)
+        {
+            try
+            {
+                var message = new System.Net.Mail.MailMessage();
+                listBoxLog.Items.Insert(0, "To: " + msgTo);
+                message.To.Add(msgTo);
+                message.Subject = "XTremeLights";
+                message.From = new System.Net.Mail.MailAddress("derek@kiwimill.com");
+                message.Body = "Your name will appear soon in lights. Check us out at facebook.com/backuslights";
+                var smtp = new System.Net.Mail.SmtpClient("smtp.gmail.com", 587);
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.Credentials = new NetworkCredential(textBoxUID.Text.ToString(), textBoxPWD.Text.ToString());
+                smtp.Send(message);
+            }
+            catch (Exception ex)
+            {
+                listBoxLog.Items.Insert(0, "SMTP Error: " + ex.Message);
+            }
+        }
+    }
+}
